d614a3a2c0fd4c5566f8e6b90377a514caa794d9
Karuna Shrestha02 <karuna100502@gmail.com>
2025-01-22 16:53:57 +0545

    added archieve in navbar

---
+++ ecs/components/nav-items/archive.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export function ArchiveNavItem() {
+  return (
+    <Link href="/archive" className="text-[#1D1B4B] hover:text-[#1D1B4B]/80 transition-colors">
+      Archive
+    </Link>
+  )
+}
+
