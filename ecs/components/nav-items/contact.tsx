4fad2d4a91955567feaa1dd3c24d844e3ac76d3e
Karuna Shrestha02 <karuna100502@gmail.com>
2025-01-22 16:54:10 +0545

    added contact feature

---
+++ ecs/components/nav-items/contact.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export function ContactNavItem() {
+  return (
+    <Link href="/contact" className="text-[#1D1B4B] hover:text-[#1D1B4B]/80 transition-colors">
+      Contact
+    </Link>
+  )
+}
+
