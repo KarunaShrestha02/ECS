18528f85d35ffab5dcb2facf7a484b321cdfc6d9
Karuna Shrestha02 <karuna100502@gmail.com>
2025-01-22 16:54:19 +0545

    added featured

---
+++ ecs/components/nav-items/featured.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export function FeaturedNavItem() {
+  return (
+    <Link href="/featured" className="text-[#1D1B4B] hover:text-[#1D1B4B]/80 transition-colors">
+      Featured
+    </Link>
+  )
+}
+
