22798d4483eb1e3d9307335dc1a336bad66a0470
Karuna Shrestha02 <karuna100502@gmail.com>
2025-01-26 17:09:31 +0545

    no message

---
+++ ecs/components/nav-items/magazine.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export function MagazineNavItem() {
+  return (
+    <Link href="/magazine" className="text-[#1D1B4B] hover:text-[#1D1B4B]/80 transition-colors">
+      Magazine
+    </Link>
+  )
+}
+
