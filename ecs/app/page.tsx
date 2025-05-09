8427dbe62dfe61f3b056c2fa1fa4944a01b312b7
Karuna Shrestha02 <karuna100502@gmail.com>
2025-05-09 23:08:47 +0545

    no message

--- ecs/app/page.tsx
+++ ecs/app/page.tsx
@@ -5,12 +5,11 @@ import { WildSection } from "@/components/wild-section"
 import { DiscoverySection } from "@/components/discovery-section"
 import { FoodSection } from "@/components/food-section"
 import { FeaturedGrid } from "@/components/featured-grid"
-import { CurrentIssue } from "@/components/currrent-issue"
-import { Footer } from "@/components/footer"
+import { LatestIssues } from "@/components/latest-issues"
+import { MagazineSlider } from "@/components/magazine-slider"
 import { SisterPublications } from "@/components/sister-publications"
 import { Newsletter } from "@/components/newsletter"
-import { MagazineSlider } from "@/components/magazine-slider"
-import { LatestIssues } from "@/components/latest-issues"
+import { Footer } from "@/components/footer"
 
 export default function Home() {
   return (
@@ -22,13 +21,11 @@ export default function Home() {
       <DiscoverySection />
       <FoodSection />
       <FeaturedGrid />
-      <LatestIssues/>
-      <MagazineSlider/>
-      <CurrentIssue />
+      <LatestIssues />
+      <MagazineSlider />
       <SisterPublications />
       <Newsletter />
       <Footer />
     </main>
   )
 }
-
