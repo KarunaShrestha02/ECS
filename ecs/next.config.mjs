4078d637cfd7270fcf138fdf53b5a517738a41d3
Karuna Shrestha02 <karuna100502@gmail.com>
2025-05-14 23:09:55 +0545

    no message

---
+++ ecs/next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["localhost", "hebbkx1anhila5yf.public.blob.vercel-storage.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    unoptimized: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
+}
+
+export default nextConfig
