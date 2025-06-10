f86f4ad16fedfc91c315df1c2e98cd9a53743516
Karuna Shrestha02 <karuna100502@gmail.com>
2025-06-10 20:43:11 +0545

    no message

--- ecs/components/nav-items/featured.tsx
+++ ecs/components/nav-items/featured.tsx
@@ -2,9 +2,8 @@ import Link from "next/link"
 
 export function FeaturedNavItem() {
   return (
-    <Link href="/featured" className="text-[#1D1B4B] hover:text-[#1D1B4B]/80 transition-colors">
+    <Link href="/featured" className="text-gray-800 hover:text-gray-600 transition-colors">
       Featured
     </Link>
   )
 }
-
